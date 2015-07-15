7f958f23e18c0009a5f2bececad1c72dc5be411a
yikaraman <yikaraman@gmail.com>
2015-07-15 12:59:44 +0300

    Datagram Socket CLient-Server Impl.

---
+++ DatagramSoketEx/src/com/yikaraman/ServerSideImplementation.java
@@ -0,0 +1,10 @@
+
+package com.yikaraman;
+
+import java.io.*;
+
+public class ServerSideImplementation {
+   public static void main(String[] args) throws IOException {
+      new ServerThread().start();
+   }
+}
